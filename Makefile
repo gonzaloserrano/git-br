2bb03a73e3a4a6738795d2c19e6de83d47a2e155
gonzaloserrano <boikot@gmail.com>
2017-06-04 20:45:18 +0200

    Change ui layour to show file differences against master

--- Makefile
+++ Makefile
@@ -1,10 +1,31 @@
+SOURCES=$(shell find . -name "*.go" | grep -v vendor/)
 PACKAGES=$(shell go list ./... | grep -v vendor/)
+FGT := fgt
 
 test:
 	go test -v $(PACKAGES)
 
 build:
 	go build cmd/git-br/git-br.go
+
+lint:
+	$(FGT) go fmt $(PACKAGES)
+	$(FGT) goimports -w $(SOURCES)
+	go list ./... | grep -v vendor/ | xargs -L1 $(FGT) golint
+	$(FGT) go vet $(PACKAGES)
+	$(FGT) errcheck -ignore Close $(PACKAGES)
+	staticcheck $(PACKAGES)
+	$(FGT) gosimple $(PACKAGES)
+
+lint-deps:
+	go get -u github.com/GeertJohan/fgt
+	go get -u golang.org/x/tools/cmd/cover
+	go get -u golang.org/x/tools/cmd/goimports
+	go get -u github.com/golang/lint/golint
+	go get -u github.com/kisielk/errcheck
+	go get -u honnef.co/go/simple/cmd/gosimple
+	go get -u github.com/mvdan/interfacer/cmd/interfacer
+	go get -u honnef.co/go/tools/cmd/staticcheck
 
 clean:
 	rm -f git-br
--- gitbr.go
+++ gitbr.go
@@ -11,6 +11,7 @@ import (
 	git "gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing"
 	"gopkg.in/src-d/go-git.v4/plumbing/object"
+	"gopkg.in/src-d/go-git.v4/utils/merkletrie"
 )
 
 // UIRunner wraps the function to Run an UI
@@ -30,17 +31,26 @@ func Open(path string) (UIRunner, error) {
 		return nil, err
 	}
 
-	return newTuiUIRunner(repo, brs), nil
+	return newTuiUI(repo, brs), nil
 }
 
 type branch struct {
 	Name   string
 	Author object.Signature
 	Branch plumbing.ReferenceName
+	Tree   *object.Tree
 }
 
-func (i branch) String() string {
-	return fmt.Sprintf("%s|%s|o %s", i.Author.When.String()[:19], i.Author.Name, i.Name)
+func (b branch) String() string {
+	author := b.Author.Name
+	if len(author) > 16 {
+		author = author[0:15] + "..."
+	}
+	name := b.Name
+	if len(name) > 32 {
+		name = name[0:31] + "..."
+	}
+	return fmt.Sprintf("%s|%s|o %s", b.Author.When.String()[2:19], author, name)
 }
 
 type branches map[string]*branch
@@ -71,61 +81,150 @@ func extract(repo *git.Repository) (branches, error) {
 	}
 
 	brsByName := make(branches)
-	brs.ForEach(func(br *plumbing.Reference) error {
+	err = brs.ForEach(func(br *plumbing.Reference) error {
 		commit, err := repo.CommitObject(br.Hash())
 		if err != nil {
 			log.Error(err.Error())
 			return nil
 		}
+		tree, err := commit.Tree()
+		if err != nil {
+			log.Error(err.Error())
+			return nil
+		}
 		name := br.Name()
-		branch := &branch{name.Short(), commit.Author, name}
+		branch := &branch{name.Short(), commit.Author, name, tree}
 		brsByName[name.Short()] = branch
 		return nil
 	})
 
+	if err != nil {
+		return nil, err
+	}
+
 	return brsByName, nil
 }
 
-func newTuiUIRunner(repo *git.Repository, brs branches) tui.UI {
-	l := tui.NewList()
-	l.SetFocused(true)
-	list := brs.displayData()
-	l.AddItems(list...)
-	l.SetSelected(0)
+func newTuiUI(repo *git.Repository, brs branches) tui.UI {
+	t := tui.NewTable(0, 0)
+
+	t.SetColumnStretch(0, 1)
+	t.SetColumnStretch(1, 1)
+	t.SetColumnStretch(2, 2)
+
+	sortedBrs := brs.sort()
+
+	for _, br := range sortedBrs {
+		author := br.Author.Name
+		if len(author) > 16 {
+			author = author[0:15] + "..."
+		}
+		name := br.Name
+		if len(name) > 26 {
+			name = name[0:25] + "..."
+		}
+		t.AppendRow(
+			tui.NewLabel(br.Author.When.String()[2:19]),
+			tui.NewLabel(author),
+			tui.NewLabel(name),
+		)
+	}
+
+	diffView := tui.NewLabel("")
 
 	status := tui.NewStatusBar("")
 	status.SetText("[press enter to switch to selected branch]")
 	status.SetPermanentText("[press esc or q to quit]")
+	top := tui.NewHBox(t, diffView)
+	top.SetSizePolicy(tui.Preferred, tui.Preferred)
 	root := tui.NewVBox(
-		l,
+		top,
 		tui.NewSpacer(),
 		status,
 	)
+	root.SetSizePolicy(tui.Preferred, tui.Preferred)
 
-	t := tui.NewTheme()
-	t.SetStyle("list.item", tui.Style{Bg: tui.ColorBlack, Fg: tui.ColorWhite})
-	t.SetStyle("list.item.selected", tui.Style{Bg: tui.ColorGreen, Fg: tui.ColorWhite})
+	th := tui.NewTheme()
+	th.SetStyle("table.cell.selected", tui.Style{Bg: tui.ColorGreen, Fg: tui.ColorWhite})
 
 	ui := tui.New(root)
-	ui.SetTheme(t)
+	ui.SetTheme(th)
 	ui.SetKeybinding(tui.KeyEsc, func() { ui.Quit() })
 	ui.SetKeybinding('q', func() { ui.Quit() })
-
-	l.OnItemActivated(func(l *tui.List) {
+	t.OnItemActivated(func(t *tui.Table) {
 		w, err := repo.Worktree()
 		if err != nil {
 			status.SetText(err.Error())
+			return
 		}
-		br := brs.sort()[l.Selected()]
+		br := sortedBrs[t.Selected()]
 		err = w.Checkout(&git.CheckoutOptions{
-			Branch: brs[br.Name].Branch,
+			Branch: br.Branch,
 			Force:  true,
 		})
+
 		if err != nil {
 			status.SetText(err.Error())
+			return
 		}
 		status.SetText("switched to " + br.Name)
 	})
+	t.OnSelectionChanged(func(t *tui.Table) {
+		br := sortedBrs[t.Selected()]
+		fromBrName := "master"
+		if br.Name == fromBrName {
+			diffView.SetText("")
+			return
+		}
+		fromBr, ok := brs[fromBrName]
+		if !ok {
+			status.SetText(fmt.Sprintf("no origin %s branch", fromBrName))
+			return
+		}
+		changes, err := object.DiffTree(fromBr.Tree, br.Tree)
+		if err != nil {
+			status.SetText(err.Error())
+			return
+		}
+		var changesMsg string
+		if len(changes) == 0 {
+			changesMsg = fmt.Sprintf("no changes between %s and %s", fromBrName, br.Name)
+		} else {
+			changesMsg = changesToString(fromBrName, changes)
+		}
+		diffView.SetText(changesMsg)
+	})
+	t.Select(0)
 
 	return ui
+}
+
+func changesToString(fromBrName string, changes object.Changes) string {
+	changesMsg := fmt.Sprintf("changes between %s and selected:\n\n", fromBrName)
+	if len(changes) > 30 {
+		changesMsg += "\ttoo many changes to display :-("
+		return changesMsg
+	}
+	for _, c := range changes {
+		action, err := c.Action()
+		if err != nil {
+			// @TODO log
+			continue
+		}
+		var actionStr, fileName string
+		switch action {
+		case merkletrie.Insert:
+			actionStr = "A"
+			fileName = c.To.Name
+		case merkletrie.Delete:
+			actionStr = "D"
+			fileName = c.From.Name
+		case merkletrie.Modify:
+			actionStr = "M"
+			fileName = c.From.Name
+		}
+		changesMsg += fmt.Sprintf("\t%s: %s\n", actionStr, fileName)
+	}
+
+	return changesMsg
 }
